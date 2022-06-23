c4466ba198e1eb9d72d84388fc684b0332c5b7bc
Malhar Patel <51440410+Unknown212-cyber@users.noreply.github.com>
2022-06-23 06:18:32 +0530

    Sketcher

---
+++ main.py
@@ -0,0 +1,35 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(20)
+
+
+def turn_left():
+    tim.left(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_right():
+    tim.right(10)
+
+
+def clear_screen():
+    screen.resetscreen()
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear_screen)
+
+screen.exitonclick()
